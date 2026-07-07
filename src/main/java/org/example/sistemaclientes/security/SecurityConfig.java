2862a1a744efec08af121b82eb17e4d8126c5bbb
Kathy Sagredo <sagredo.kathy@gmail.com>
2026-07-07 10:21:34 -0400

    Eliminación de librerias import

--- src/main/java/org/example/sistemaclientes/security/SecurityConfig.java
+++ src/main/java/org/example/sistemaclientes/security/SecurityConfig.java
@@ -1,7 +1,5 @@
 package org.example.sistemaclientes.security;
 
-import lombok.AllArgsConstructor;
-import org.example.sistemaclientes.dto.ClienteRequestDTO;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -12,12 +10,8 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
